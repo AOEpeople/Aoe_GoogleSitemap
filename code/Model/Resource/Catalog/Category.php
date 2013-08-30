cda8ba8c9638266c7910cc5d0ae96f8d63d0e03b
Martin <m.witczak@macopedia.pl>
2013-08-30 15:38:27 +0200

    added sitemap categories filtering based on a new category attribute sitemap_exclude

---
+++ code/Model/Resource/Catalog/Category.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Magento Enterprise Edition
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Magento Enterprise Edition License
+ * that is bundled with this package in the file LICENSE_EE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.magentocommerce.com/license/enterprise-edition
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Sitemap
+ * @copyright   Copyright (c) 2012 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://www.magentocommerce.com/license/enterprise-edition
+ */
+
+
+/**
+ * Sitemap resource catalog collection model
+ *
+ * @category    Mage
+ * @package     Mage_Sitemap
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Aoe_GoogleSitemap_Model_Resource_Catalog_Category extends Mage_Sitemap_Model_Resource_Catalog_Category
+{
+    public function getCollection($storeId)
+    {
+        $categories = array();
+
+        $store = Mage::app()->getStore($storeId);
+        /* @var $store Mage_Core_Model_Store */
+
+        if (!$store) {
+            return false;
+        }
+
+        $this->_select = $this->_getWriteAdapter()->select()
+            ->from($this->getMainTable())
+            ->where($this->getIdFieldName() . '=?', $store->getRootCategoryId());
+        $categoryRow = $this->_getWriteAdapter()->fetchRow($this->_select);
+
+        if (!$categoryRow) {
+            return false;
+        }
+
+        $urConditions = array(
+            'e.entity_id=ur.category_id',
+            $this->_getWriteAdapter()->quoteInto('ur.store_id=?', $store->getId()),
+            'ur.product_id IS NULL',
+            $this->_getWriteAdapter()->quoteInto('ur.is_system=?', 1),
+        );
+        $this->_select = $this->_getWriteAdapter()->select()
+            ->from(array('e' => $this->getMainTable()), array($this->getIdFieldName()))
+            ->joinLeft(
+                array('ur' => $this->getTable('core/url_rewrite')),
+                join(' AND ', $urConditions),
+                array('url'=>'request_path')
+            )
+            ->where('e.path LIKE ?', $categoryRow['path'] . '/%');
+
+        $this->_addFilter($storeId, 'is_active', 1);
+        //Added filtering for sitemap_exclude attribute - Martin.W
+        $this->_addFilter($storeId, 'sitemap_exclude', 0);
+        //---------------------------------------------
+        $query = $this->_getWriteAdapter()->query($this->_select);
+        while ($row = $query->fetch()) {
+            $category = $this->_prepareCategory($row);
+            $categories[$category->getId()] = $category;
+        }
+
+        return $categories;
+    }
+}
--- code/sql/aoegooglesitemap_setup/mysql4-install-1.0.0.php
+++ code/sql/aoegooglesitemap_setup/mysql4-install-1.0.0.php
@@ -3,6 +3,17 @@ $installer = $this; /* @var $installer Mage_Core_Model_Resource_Setup */
 
 $installer->startSetup();
 
+$installer->addAttribute('catalog_category', 'sitemap_exclude', array(
+    'group'    => 'General Information',
+    'input'    => 'select',
+    'type'     => 'text',
+    'label'    => 'Exclude from Sitemap',
+    'source'   => 'eav/entity_attribute_source_boolean',
+    'required' => 0,
+    'global'   => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'note'     => 'Exclude from xml sitemap generation',
+    'default'  => 0
+));
 $dbStorage = Mage::helper('core/file_storage_database')->getStorageDatabaseModel(); /* @var $dbStorage Mage_Core_Model_File_Storage_Database */
 $dbStorage->getDirectoryModel()->prepareStorage();
 $dbStorage->prepareStorage();
